f96f17f36174f5e3a31db274702b9628a523dc41
Sumedh Wale <sumwale@gmail.com>
2021-05-18 18:57:17 +0530

    few config changes

--- .local/bin/touchpad-settings.sh
+++ .local/bin/touchpad-settings.sh
@@ -4,5 +4,5 @@ touchpad="SynPS/2 Synaptics TouchPad"
 xinput set-prop "$touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "$touchpad" "libinput Natural Scrolling Enabled" 1
 xinput set-prop "$touchpad" "libinput Disable While Typing Enabled" 1
-( sleep 5 && xbacklight -set 50 ) &
+( sleep 5 && xbacklight -set 60 ) &
 #xgamma -gamma 0.90
