b497c74b5c7ba1b1e3822cd1e304d659db1692a8
Sumedh Wale <sumwale@gmail.com>
2021-12-05 16:29:59 +0530

    updated cloudera build
    - update NSS and deps explicitly before starting yum due to the ancient CentOS6 packages
    - updated build scripts and deps for python 3.9.9
    - updated docker-cleanup to skip exited containers by default

--- cloudera/run.sh
+++ cloudera/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
 docker build -t local/cloudera-quickstart:latest .
-docker-cleanup
+docker-cleanup -f
 docker run --hostname=quickstart.cloudera --privileged=true --network=host --name=cloudera -itd local/cloudera-quickstart:latest
