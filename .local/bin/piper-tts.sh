f29309ba728104740658dd85ad401467bc8b9428
Sumedh Wale <sumwale@gmail.com>
2023-12-19 22:49:35 +0530

    add piper-tts configuration for speech-dispatcher

---
+++ .local/bin/piper-tts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+VOICE_PATH="$HOME/Downloads/piper-tts"
+
+if [[ ${VOICE: -3} = low ]]; then
+  ADJ_RATE=16000
+else
+  ADJ_RATE=22050
+fi
+if [[ ${#RATE} -gt 3 ]]; then
+  ADJ_RATE=$((${RATE::-3} * 30 + $ADJ_RATE))
+fi
+mkdir -p "$VOICE_PATH"
+echo "$DATA" | piper --model $VOICE_PATH/$VOICE.onnx  --output-raw | \
+  aplay -q -r $ADJ_RATE -f S16_LE -t raw -
+
+wait
