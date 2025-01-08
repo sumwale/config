85502af9468604e0c328d3392132dc0176cf7e82
Sumedh Wale <sumwale@gmail.com>
2025-01-09 01:16:31 +0530

    updated icons, minor updates to virt-st* scripts and borgmatic config

--- .local/bin/virt-start.sh
+++ .local/bin/virt-start.sh
@@ -4,6 +4,8 @@
 
 sudo modprobe kvm_intel
 
-for service in qemu-kvm.service libvirtd-ro.socket libvirtd.socket libvirtd-admin.socket virtlockd-admin.socket virtlogd-admin.socket virtlogd.socket virtlockd.socket libvirt-guests.service libvirtd.service rpcbind nfs-kernel-server nfs-server sshd; do
-  sudo systemctl restart $service
+for service in qemu-kvm.service libvirtd-ro.socket libvirtd.socket libvirtd-admin.socket virtlockd-admin.socket virtlogd-admin.socket virtlogd.socket virtlockd.socket libvirt-guests.service libvirtd.service rpcbind.service nfs-kernel-server.service nfs-server.service smb.service nmb.service smbd.service nmbd.service ssh.service sshd.service; do
+  if sudo systemctl list-unit-files $service; then
+    sudo systemctl restart $service
+  fi
 done
