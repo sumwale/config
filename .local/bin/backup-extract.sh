d8d3531f67c2abbfa689cb0221d9ba8bd2472d94
Sumedh Wale <sumwale@gmail.com>
2025-01-26 03:39:42 +0530

    updated hard-coded USER and HOME in borgmatic config
    - use $USER and $HOME in borgmatic config.yaml instead of hardcoding the values
    - updates to backup-* scripts as per current ones on the backup server

--- .local/bin/backup-extract.sh
+++ .local/bin/backup-extract.sh
@@ -5,4 +5,4 @@ output_dir=$3
 
 # top-level directories are created with default root ownership and need to have read
 # and execute access to the users
-sudo /bin/sh -c "cd $output_dir && PATH=$PATH:/root/.local/bin borg extract --progress --numeric-ids --sparse $HOME/backup::$1 $2; chmod 755 * boot/grub home/sumedh usr/bin usr/share usr/share/* usr/share/libdvd-pkg/debian 2>/dev/null"
+sudo /bin/sh -c "cd $output_dir && PATH=$PATH:/root/.local/bin borg extract --progress --sparse $HOME/backup::$1 $2; chmod 755 * boot/grub home/sumedh usr/bin usr/share usr/share/* usr/share/libdvd-pkg/debian 2>/dev/null"
--- .local/bin/backup-sync.sh
+++ .local/bin/backup-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-sudo rsync -aHSAXX --filter='-x security.selinux' --info=progress2 --delete --zc=zstd --zl=6 ~/mnt/. ~/backup.sync/.
+sudo rsync -aHSA --info=progress2 --delete --zc=zstd --zl=6 $HOME/mnt/ $HOME/backup.sync/
