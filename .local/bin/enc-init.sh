57c7e302464493ab77b82c3193531643e790241d
Sumedh Wale <sumwale@gmail.com>
2025-01-28 08:44:25 +0530

    revamped sync-data.sh to work for mounted root
    - many updates to sync-data.sh to work correctly for full restore/sync on a mounted root
      (e.g. from a live USB/CD session)
    - don't remove fprintd pam-config if fprintd is not installed, rather copy to
      /usr/local/pam-configs/fprintd.bak so that backup always has a copy of the
      fprintd PAM configuration
    - updated enc-init.sh to ask for passwords only once and pass those along to fscrypt
      encryption; use gpg to temporarily save the passwords after encryption rather than
      storing in a bash variable
    - add /usr/share/keyrings to the backup excluding standard ubuntu ones

--- .local/bin/enc-init.sh
+++ .local/bin/enc-init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -8,37 +8,79 @@ set -e
 # which should only be run either by root when the user is not logged in, or in
 # text-mode login by user ensuring that no other processes are running (kill -9 -1).
 
+login_protector=
+login_pass_read=0
+other_pass_read=0
+login_pass_file=/tmp/fscrypt-enc-init1.gpg
+other_pass_file=/tmp/fscrypt-enc-init2.gpg
+gpg_id=sumwale@gmail.com
+
+trap "stty echo; shred -u $login_pass_file $other_pass_file 2>/dev/null" \
+  0 1 2 3 4 5 6 7 8 10 11 12 14 15
+
+if [ "$1" = --tty ]; then
+  decrypt_opts="--decrypt --pinentry-mode loopback --quiet" # ask password from the terminal
+else
+  decrypt_opts="--decrypt --quiet"
+fi
+
 ENC_PREFIX="enc-init"
-for dir in `cat enc.dirs`; do
+for dir in $(cat enc.dirs); do
   if [ ! -e $dir.$ENC_PREFIX ] && ! fscrypt status $dir >/dev/null 2>/dev/null; then
+    if [ $login_pass_read -eq 0 ]; then
+      [ -f $login_pass_file ] && shred -u $login_pass_file
+      echo -n "Enter the login passphrase for $USER: "
+      stty -echo
+      head -n1 | gpg -r $gpg_id --encrypt --batch -o $login_pass_file
+      stty echo
+      echo
+      echo "Encrypted the passphrase using 'gpg -r $gpg_id' (gpg passphrase may be asked later)"
+      login_pass_read=1
+    fi
+    if [ -z "$login_protector" ]; then
+      login_protector=$(fscrypt status / | sed -n 's/^\(\S*\).*login protector.*$/\1/p')
+      if [ -z "$login_protector" ]; then
+        protector_arg="--source=pam_passphrase"
+      fi
+    fi
+    if [ -n "$login_protector" ]; then
+      protector_arg="--protector=/:$login_protector"
+    fi
     echo Encrypting $dir...
-    mkdir -p $dir $dir.$ENC_PREFIX && \
-      fscrypt encrypt $dir.$ENC_PREFIX --no-recovery && \
-      cp -a -T $dir $dir.$ENC_PREFIX
+    mkdir -p $dir $dir.$ENC_PREFIX
+    gpg $decrypt_opts $login_pass_file |
+      fscrypt encrypt $dir.$ENC_PREFIX --quiet --no-recovery $protector_arg
+    cp -a -T $dir $dir.$ENC_PREFIX
   fi
 done
 
 # create an additional protector that can be used when login protector
 # is not available (e.g. root filesystem has changed)
-if findmnt /home 2>/dev/null >/dev/null; then
-  HOME_MNT=/home
-else
-  HOME_MNT=/
+dir_mnt=$(findmnt -n -o TARGET -T .)
+if ! fscrypt status $dir_mnt | grep -qF 'custom protector'; then
+  fscrypt metadata create protector $dir_mnt
 fi
+custom_protector=($(fscrypt status $dir_mnt |
+  sed -n 's/^\(\S*\).*custom protector\s*"\(\S*\)".*$/\1 \2/p'))
 
-NEW_PROTECTOR="`fscrypt status $HOME_MNT | sed -n 's/^\([^ \t]*\).*custom protector.*$/\1/p'`"
-if [ -z "$NEW_PROTECTOR" ]; then
-  fscrypt metadata create protector $HOME_MNT
-fi
-NEW_PROTECTOR="`fscrypt status $HOME_MNT | sed -n 's/^\([^ \t]*\).*custom protector.*$/\1/p'`"
-
-if [ -n "$NEW_PROTECTOR" ]; then
-  for dir in `cat enc.dirs`; do
+if [ ${#custom_protector[@]} -eq 2 ]; then
+  for dir in $(cat enc.dirs); do
     if [ -d $dir.$ENC_PREFIX ]; then
-      POLICY="`fscrypt status $dir.$ENC_PREFIX | sed -n 's/^.*Policy:[ ]*\([^ \t]*\).*$/\1/p'`"
-      if [ -n "$POLICY" ]; then
+      policy_id=$(fscrypt status $dir.$ENC_PREFIX | sed -n 's/^.*Policy:\s*\(\S*\).*$/\1/p')
+      if [ -n "$policy_id" ]; then
+        if [ $other_pass_read -eq 0 ]; then
+          [ -f $other_pass_file ] && shred -u $other_pass_file
+          echo -n "Enter the passphrase for protector with name '${custom_protector[1]}': "
+          stty -echo
+          head -n1 | gpg -r $gpg_id --encrypt --batch -o $other_pass_file
+          stty echo
+          echo
+          other_pass_read=1
+        fi
         echo Adding additional protector to $dir...
-        fscrypt metadata add-protector-to-policy --protector=$HOME_MNT:$NEW_PROTECTOR --policy=$HOME_MNT:$POLICY
+        { gpg $decrypt_opts $other_pass_file && gpg $decrypt_opts $login_pass_file; } |
+          fscrypt metadata add-protector-to-policy --quiet \
+            --protector=$dir_mnt:${custom_protector[0]} --policy=$dir_mnt:$policy_id
       fi
     fi
   done
--- .local/bin/sync-data.sh
+++ .local/bin/sync-data.sh
@@ -13,10 +13,10 @@
 # The installation can be an existing one being used or an entirely fresh one.
 # The script makes no assumption of any existing data on the system apart from
 # a minimal Ubuntu based one having dpkg/apt (optionally apt-fast) and basic
-# utilities rsync, ssh, ssh-agent with ssh-add, gpg, git (and basic ones
+# utilities rsync, ssh, ssh-agent with ssh-add, gpg, git, curl (and basic ones
 #   like sudo/awk/sed/comm that are present in all usable installations).
 # It makes use of mrpsync.sh script to launch parallel rsync processes which is
-# downloaded, if not present, from the github repository.
+# downloaded, if not present, from its github repository.
 
 set -e
 
@@ -31,15 +31,18 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 sync_data_conf=/tmp/sync-data
 log_file=/tmp/sync-data.log
 err_file=/tmp/sync-data.err
+pkgs_err=/tmp/sync-data-pkgs.err
 
 if [ "$SCRIPT_DIR" != "/tmp" ]; then
   cp -f "$SCRIPT_DIR/$SCRIPT" /tmp/
+  cp -f "$SCRIPT_DIR/enc-init.sh" "$SCRIPT_DIR/enc-finish.sh" /tmp/
   cp -rfL "$SCRIPT_DIR/../../.config/sync-data" $sync_data_conf
-  exec /bin/bash /tmp/$SCRIPT "$@" > >(tee $log_file) 2> >(tee $err_file)
+  /bin/bash /tmp/$SCRIPT "$@" > >(tee $log_file) 2> >(tee $err_file)
   exit $?
 fi
 
 sync_root=
+sync_user=
 home_dir=$HOME
 def_remote_root=$USER@$BORG_BACKUP_SERVER:backup.sync
 fg_red='\033[31m'
@@ -48,37 +51,55 @@ fg_orange='\033[33m'
 fg_reset='\033[00m'
 rsync_common_options='-aHSOJ --info=progress2 --zc=zstd --zl=1'
 sync_enc_data=/tmp/others.txz
+ssh_key_created=0
+auth_keys_orig=authorized_keys.sync-bak
 ssh_agent_started=0
+unpack_gpg_key=0
 
 function usage() {
   echo
-  echo "Usage: $SCRIPT [--home=HOME] [<SOURCE_ROOT>] [<DEST_ROOT>]"
+  echo "Usage: $SCRIPT [--help] [--user=USER] [<SOURCE_ROOT>] [<DEST_ROOT>]"
   echo
-  echo "--home=HOME      home directory for the user on source and destination if "
-  echo "                 different from '$HOME'"
+  echo "--help           show this usage"
+  echo "--user=USER      user name on source and destination if different from '$USER'"
   echo
   echo "  SOURCE_ROOT     source location for the sync (default: $def_remote_root)"
   echo "  DEST_ROOT       desination root directory to sync; empty means /"
   echo
-  echo "NOTE: the script assumes that neither of the two args have whitespace characters"
+  echo "NOTE: the script assumes that none of the argument values have whitespace characters"
 }
 
 function cleanup() {
-  rm -rf /tmp/$SCRIPT $sync_data_conf $home_dir/enc.dirs.new /tmp/mprsync.sh
+  echo -e "${fg_red}Cleaning up...$fg_reset"
+  rm -rf /tmp/$SCRIPT /tmp/enc-init.sh /tmp/enc-finish.sh $sync_data_conf /tmp/mprsync.sh $pkgs_err
   [ -e $sync_enc_data ] && shred -u $sync_enc_data
   if [ -f $home_dir/enc.dirs.orig ]; then
-    rm -f $home_dir/enc.dirs
-    mv $home_dir/enc.dirs.orig $home_dir/enc.dirs
+    sudo rm -f $home_dir/enc.dirs
+    sudo mv $home_dir/enc.dirs.orig $home_dir/enc.dirs
+  fi
+  if [ $ssh_key_created -eq 1 -a -n "$keyfile" ]; then
+    remote_server=${remote_root%:*}
+    ssh $remote_server mv -f .ssh/$auth_keys_orig .ssh/authorized_keys
+    ssh-add -d $keyfile
+    rm -f $keyfile*
   fi
   if [ $ssh_agent_started -eq 1 ]; then
     kill $SSH_AGENT_PID
+  fi
+  if [ $unpack_gpg_key -eq 1 -a $home_dir != $HOME ]; then
+    find $HOME/.gnupg -type f -print0 | xargs -0 shred -n1 -u
+    rm -rf $HOME/.gnupg
   fi
 }
 
 # argument processing
 
-if [[ "$1" = --home=* ]]; then
-  home_dir="${1/*=/}"
+if [ "$1" = "--help" ]; then
+  usage
+  exit 1
+fi
+if [[ "$1" = --user=* ]]; then
+  sync_user="${1/*=/}"
   shift
 fi
 [ $# -gt 2 ] && { echo -e "${fg_red}Unexpected args:" "$@"$fg_reset; usage; exit 1; }
@@ -97,16 +118,30 @@ else
   remote_root=$def_remote_root
 fi
 [ $# -eq 2 -a "$2" != "/" ] && sync_root=$2
+sync_root_fs=${sync_root:-/}
 
-remote_home=$remote_root$home_dir
-home_dir=$sync_root$home_dir
 if [ -n "$sync_root" ]; then
   divert_root_arg="--root $sync_root"
   dpkg_root_arg="--root=$sync_root"
-  apt_root_arg="-o RootDir=$sync_root"
+  chroot_arg="chroot $sync_root"
+  if [ -n "$sync_user" ]; then
+    home_dir=$(sudo $chroot_arg getent passwd $sync_user | cut -d: -f6)
+    echo -e "${fg_green}Using '$home_dir' as the HOME for backup user '$sync_user'$fg_reset"
+    echo
+  else
+    sync_user=$USER
+  fi
+  sync_user_group=$(sudo $chroot_arg id -gn $sync_user)
+  chroot_arg_user="sudo chroot --userspec=$sync_user:$sync_user_group $sync_root"
+else
+  sync_user=${sync_user:-$USER}
+  sync_user_group=$(id -gn $sync_user)
 fi
+remote_home=$remote_root$home_dir
+home_dir_local=$home_dir
+home_dir=$sync_root$home_dir
 
-trap "cleanup" 0 1 2 3 4 5 6 11 12 15
+trap "cleanup" 0 1 2 3 4 5 6 7 8 10 11 12 14 15
 
 # First deal with package modifications since they may have new files from backup
 # which should be overwritten by the backup (else if /etc file is synced from backup,
@@ -114,6 +149,12 @@ trap "cleanup" 0 1 2 3 4 5 6 11 12 15
 
 echo -e "${fg_green}Output logged to $log_file, errors to $err_file.$fg_reset"
 echo
+
+# Install or update packages used by the script
+
+sudo apt-get update
+sudo apt-get install -y rsync gpg openssh-client coreutils util-linux mawk sed tar curl sudo
+sudo apt-get install -y apt dpkg
 
 # Start ssh-agent if it does not exist and set it up
 
@@ -128,15 +169,28 @@ fi
 AWK=awk
 type -p mawk >/dev/null && AWK=mawk
 # check and add keys if not added (only RSA and ED25519 keys are checked)
-if [ -r $home_dir/.ssh/id_rsa ]; then
-  keyfile=$home_dir/.ssh/id_rsa
-elif [ -r $home_dir/.ssh/id_ed25519 ]; then
-  keyfile=$home_dir/.ssh/id_ed25519
+if [ -r $HOME/.ssh/id_rsa ]; then
+  keyfile=$HOME/.ssh/id_rsa
+elif [ -r $HOME/.ssh/id_ed25519 ]; then
+  keyfile=$HOME/.ssh/id_ed25519
+else
+  remote_server=${remote_root%:*}
+  echo -en "${fg_orange}No ssh private key found for use on ssh server. "
+  echo "Without public key auth, every rsync process will ask for password."
+  echo -en "Should one be generated and added to '$remote_server' (y/N)? $fg_reset"
+  if read resp && [ "$resp" = y -o "$resp" = Y ]; then
+    keyfile=$HOME/.ssh/id_ed25519
+    ssh-keygen -o -a 100 -t ed25519 -f $keyfile
+    cat $keyfile.pub | ssh -o PubkeyAuthentication=no $remote_server \
+      "mv .ssh/authorized_keys .ssh/$auth_keys_orig &&
+      cat .ssh/$auth_keys_orig - > .ssh/authorized_keys && chmod 400 .ssh/authorized_keys"
+    ssh_key_created=1
+  fi
 fi
 if [ -n "$keyfile" ]; then
   fp=$(ssh-keygen -l -f $keyfile | $AWK '{ print $2 }')
   if [ -z "$fp" ] || ! ssh-add -l | grep -qwF "$fp"; then
-    ssh-add
+    ssh-add $keyfile
   fi
 fi
 
@@ -145,17 +199,63 @@ fi
 # can be obtained now and user can come back leisurely after the full sync is done
 # (else gnupg password popup can timeout so user will need to keep an eye on sync being finished)
 
-echo -e "${fg_green}Updating package lists, sync packages and fetch encrypted data.$fg_reset"
-APT_FAST=apt
-type -p apt-fast >/dev/null && APT_FAST=apt-fast
-$APT_FAST update || true
+APT_FAST=apt-get
+if sudo $chroot_arg which apt-fast >/dev/null; then
+  APT_FAST=apt-fast
+elif sudo $chroot_arg which add-apt-repository >/dev/null; then
+  echo -e "${fg_green}Installing apt-fast for faster downloads.$fg_reset"
+  sudo $chroot_arg add-apt-repository ppa:apt-fast/stable
+  sudo $chroot_arg apt-get update || true
+  sudo DEBIAN_FRONTEND=noninteractive $chroot_arg apt-get install -y apt-fast
+  APT_FAST=apt-fast
+fi
 
-rsync $rsync_common_options --delete $remote_home/pkgs \
-  $remote_home/Documents/others.key.gpg $home_dir/
+echo -e "${fg_green}Updating package lists, sync packages and fetch encrypted data.$fg_reset"
+
+# If gpg secret key is not present, then fetch the .gnupg encrypted tar
+
+enc_bundles=$remote_home/Documents/others.key.gpg
+if ! gpg --quiet --list-secret-key sumwale@gmail.com >/dev/null 2>/dev/null; then
+  enc_bundles="$enc_bundles $remote_home/Documents/rest.key.gpg"
+  unpack_gpg_key=1
+fi
+# most rsync calls will use sudo since the current user's UID may be different from that
+# of the backup user whose data is being restored; '-E' option is to enable using
+# current user's ssh key for remote server public key authentication
+rsync $rsync_common_options --delete $remote_home/pkgs $enc_bundles $HOME/
 sudo -E rsync $rsync_common_options --exclude-from=$sync_data_conf/excludes-root.list \
   $remote_root/etc/apt/ $sync_root/etc/apt/
-gpg --decrypt $home_dir/others.key.gpg > $sync_enc_data
-shred -u $home_dir/others.key.gpg
+sudo -E rsync $rsync_common_options --exclude-from=$sync_data_conf/excludes-root.list \
+  $remote_root/usr/share/keyrings/ $sync_root/usr/share/keyrings/
+# check for ubuntu pro repositories and if they are accessible, else remove them
+if compgen -G "$sync_root/etc/apt/sources.list.d/ubuntu-esm-*" >/dev/null; then
+  pro_status=$(sudo $chroot_arg pro status --format=yaml | $AWK '/attached:/ { print $2 }')
+  if [ "$pro_status" != true ]; then
+    echo -e "${fg_orange}Removing Ubuntu Pro repositories since it is not enabled.$fg_reset"
+    sudo rm -f $sync_root/etc/apt/sources.list.d/ubuntu-esm-*
+  fi
+fi
+# enable 32-bit packages and update package lists after the changes to apt configuration
+sudo $chroot_arg dpkg --add-architecture i386
+sudo $chroot_arg apt-get update || true
+
+if [ $unpack_gpg_key -eq 1 ]; then
+  rm -rf $HOME/.gnupg/*
+  gpg --decrypt $HOME/rest.key.gpg | tar --strip-components=2 -C $HOME -xpSJf -
+  shred -u $HOME/rest.key.gpg
+  # copy over gnupg keys from host setup if required and link gpg.conf to the one in config repo
+  if [ $home_dir != $HOME ]; then
+    $chroot_arg_user mkdir -p $home_dir_local/.gnupg
+    $chroot_arg_user chmod 0700 $home_dir_local/.gnupg
+    sudo rm -rf $home_dir/.gnupg/*
+    sudo cp -a $HOME/.gnupg/* $home_dir/.gnupg/.
+    sudo rm -f $home_dir/.gnupg/gpg.conf
+    sudo ln -s ../config/.gnupg/gpg.conf $home_dir/.gnupg/gpg.conf
+    sudo $chroot_arg chown -R $sync_user:$sync_user_group $home_dir_local/.gnupg
+  fi
+fi
+gpg --decrypt $HOME/others.key.gpg > $sync_enc_data
+shred -u $HOME/others.key.gpg
 
 # Apply diversions recorded in the backup.
 
@@ -164,7 +264,7 @@ $AWK -v root=$sync_root -v root_arg="$divert_root_arg" '{
   diversion = $0; getline
   divert_to = $0; getline
   if ($0 == ":") {
-    system("sudo /bin/sh -c \"mv -f " root diversion " " root diversion ".divert; " \
+    system("sudo /bin/sh -c \"mv -f " root diversion " " root diversion ".divert 2>/dev/null; " \
       "dpkg-divert " root_arg " --local --remove --rename --divert " divert_to " " diversion "\"")
   }
 }' $sync_root/var/lib/dpkg/diversions
@@ -177,18 +277,21 @@ $AWK -v root=$sync_root -v root_arg="$divert_root_arg" '{
     system("sudo /bin/sh -c \"[ -f " diversion ".divert ] && mv -f " \
       diversion ".divert " diversion "\"")
   }
-}' $home_dir/pkgs/deb-diversions
+}' $HOME/pkgs/deb-diversions
 
 # Compare host machine packages with the backup and offer to apply any changes.
 
+# first fix any broken stuff
+sudo DEBIAN_FRONTEND=noninteractive $chroot_arg dpkg --configure -a
+sudo DEBIAN_FRONTEND=noninteractive $chroot_arg apt-get install -f --purge
 echo -e "${fg_green}Comparing packages on this host with the backup.$fg_reset"
-new_pkgs=$(sed -n 's/^ii\s\+\(\S\+\).*$/\1/p' $home_dir/pkgs/deb.list | sort)
+new_pkgs=$(sed -n 's/^ii\s\+\(\S\+\).*$/\1/p' $HOME/pkgs/deb.list | sort)
 # "apt-mark showinstall" does not include arch in package names, so better to use
 # "dpkg -l" rather than curating their outputs for arch
 pkg_diffs=$(comm -3 <(echo "$new_pkgs") \
-                    <(dpkg -l $dpkg_root_arg | sed -n 's/^ii\s\+\(\S\+\).*$/\1/p' | sort) \
-  | grep -vwE 'fprintd|libfprint|command-configure|srvadmin' \
-  | sed 's/^\</INSTALL: /;s/^\s\+/PURGE: /')
+                    <(dpkg -l $dpkg_root_arg | sed -n 's/^ii\s\+\(\S\+\).*$/\1/p' | sort) |
+  grep -vwE 'fprintd|libfprint|command-configure|srvadmin' |
+  sed 's/^\</INSTALL: /;s/^\s\+/PURGE: /')
 if [ -n "$pkg_diffs" ]; then
   echo -e "${fg_orange}Changes detected, see the following package modifications.$fg_reset"
   install_pkgs=$(echo "$pkg_diffs" | sed -n 's/^INSTALL: //p')
@@ -202,37 +305,89 @@ if [ -n "$pkg_diffs" ]; then
   echo -en "${fg_orange}Perform the above changes (y/N)? $fg_reset"
   if read resp && [ "$resp" = y -o "$resp" = Y ]; then
     if [ -n "$install_pkgs" ]; then
-      $APT_FAST $apt_root_arg install $install_pkgs
+      # unfortunately apt provides no way to skip unavailable packages (e.g. separately downloaded
+      #   in deb-local and elsewhere), so need to check for the available ones
+      selected_pkgs=$(sudo $chroot_arg apt-cache -q0 show $install_pkgs 2> $pkgs_err |
+        $AWK '/^Package: / {
+          pkg = $2
+          while (getline != 0) {
+            if ($1 == "Architecture:") {
+              arch = $2
+              break
+            }
+          }
+          if (!arch) print pkg
+          else print pkg ":" arch
+        }' | uniq) # using uniq to filter dups is much faster than --no-all-versions
+      if [ -s $pkgs_err ]; then
+        skipped_pkgs=$(sed -n \
+          "s/.* package '\([^']*\)'.*/\1/p;s/.*Unable to locate.* \(\S\+\)$/\1/p" $pkgs_err)
+        echo -e "${fg_red}Skipping the following unavailable packages: "$skipped_pkgs$fg_reset
+      fi
+      if [ -n "$selected_pkgs" ]; then
+        sudo DEBIAN_FRONTEND=noninteractive $chroot_arg $APT_FAST install -y --purge $selected_pkgs
+      fi
     fi
     if [ -n "$purge_pkgs" ]; then
-      sudo apt $apt_root_arg purge $purge_pkgs
+      sudo DEBIAN_FRONTEND=noninteractive $chroot_arg apt-get purge -y $purge_pkgs
     fi
     # mark the ones in deb-explicit.list as manually installed while the rest as auto
-    sudo apt-mark $apt_root_arg auto $new_pkgs || true
-    sudo apt-mark $apt_root_arg manual $(cat $home_dir/pkgs/deb-explicit.list) || true
+    sudo $chroot_arg apt-mark auto $new_pkgs || true
+    sudo $chroot_arg apt-mark manual $(cat $HOME/pkgs/deb-explicit.list) || true
+    sudo DEBIAN_FRONTEND=noninteractive $chroot_arg apt-get autopurge || true
   fi
 fi
+sudo DEBIAN_FRONTEND=noninteractive $chroot_arg $APT_FAST dist-upgrade --purge || true
+sudo $chroot_arg $APT_FAST clean
+rm -rf $HOME/pkgs
 
 # Encrypt any new directories listed in backup
 
-rsync $rsync_common_options $remote_home/enc.dirs $home_dir/enc.dirs.new
-if ! cmp $home_dir/enc.dirs $home_dir/enc.dirs.new >/dev/null 2>/dev/null; then
+[ ! -e $home_dir/enc.dirs ] && $chroot_arg_user touch $home_dir_local/enc.dirs
+$chroot_arg_user mv -f $home_dir_local/enc.dirs $home_dir_local/enc.dirs.orig
+sudo -E rsync $rsync_common_options $remote_home/enc.dirs $home_dir/
+sudo cp -f $SCRIPT_DIR/enc-init.sh $SCRIPT_DIR/enc-finish.sh $home_dir/
+sudo chmod 0755 $home_dir/enc-*.sh
+if ! sudo cmp $home_dir/enc.dirs.orig $home_dir/enc.dirs >/dev/null 2>/dev/null; then
   echo -e "${fg_green}Following new directories require encryption:$fg_reset"
-  comm -13 <(sort $home_dir/enc.dirs) <(sort $home_dir/enc.dirs.new)
+  comm -13 <(sudo sort $home_dir/enc.dirs.orig) <(sudo sort $home_dir/enc.dirs)
   home_mnt=$(findmnt -n -o TARGET --target $home_dir)
-  sync_root_fs=${sync_root:-/}
   echo
   echo -n "Encryption will work only if '$sync_root_fs' has already been setup by "
   echo -n "fscrypt (as well as '$home_mnt' if different) and filesystems have been "
   echo -n "marked for encryption (e.g. tune2fs -O encrypt /dev/... for ext4). "
+  echo -n "This script will try to do this setup if not detected but if that fails,"
+  echo -n "then do it manually first and then proceed."
+  echo
   echo -n "The PAM configuration for auto-unlocking directories using your login "
   echo -n "password should be copied during the sync from the backup."
   echo
   echo -en "${fg_orange}Encrypt the above directories (y/N)? $fg_reset"
   if read resp && [ "$resp" = y -o "$resp" = Y ]; then
-    mv -f $home_dir/enc.dirs $home_dir/enc.dirs.orig
-    mv $home_dir/enc.dirs.new $home_dir/enc.dirs
-    ( cd $home_dir && enc-init.sh && enc-finish.sh && rm -f $home_dir/enc.dirs.orig )
+    # check if fscrypt needs to be setup for root and possibly home mount
+    if ! sudo $chroot_arg which fscrypt >/dev/null; then
+      sudo DEBIAN_FRONTEND=noninteractive $chroot_arg $APT_FAST install -y --purge libpam-fscrypt
+    fi
+    if ! sudo $chroot_arg fscrypt status / --quiet 2>/dev/null; then
+      # set encrypt flag for ext4
+      if [ $(findmnt -n -o FSTYPE --target $sync_root_fs) = ext4 ]; then
+        sudo tune2fs -O encrypt $(findmnt -n -o SOURCE --target $sync_root_fs)
+      fi
+      sudo $chroot_arg fscrypt setup --all-users
+    fi
+    if [ $sync_root_fs != $home_mnt ]; then
+      home_mnt=${home_mnt#$sync_root_fs}
+      if ! sudo $chroot_arg fscrypt status $home_mnt --quiet 2>/dev/null; then
+        # set encrypt flag for ext4
+        if [ $(findmnt -n -o FSTYPE --target $sync_root_fs$home_mnt) = ext4 ]; then
+          sudo tune2fs -O encrypt $(findmnt -n -o SOURCE --target $sync_root_fs$home_mnt)
+        fi
+        sudo $chroot_arg fscrypt setup $home_mnt --all-users
+      fi
+    fi
+    $chroot_arg_user /bin/bash -c "export HOME=$home_dir_local && export USER=$sync_user &&
+      export LOGNAME=$sync_user && cd $home_dir_local && ./enc-init.sh --tty && ./enc-finish.sh"
+    sudo rm -f $home_dir/enc-init.sh $home_dir/enc-finish.sh $home_dir/enc.dirs.orig
   fi
 fi
 
@@ -257,18 +412,24 @@ sudo -E rsync $rsync_common_options --exclude-from=$sync_data_conf/excludes-root
   $remote_root/boot $remote_root/etc $remote_root/usr $sync_root/
 
 echo -e "${fg_green}Unpacking and writing encrypted data.$fg_reset"
-sudo tar --xz -C $sync_root/ --overwrite -xpSf $sync_enc_data
-if [ -z "$sync_root" ]; then
-  sudo update-grub
-else
-  echo -e "${fg_orange}Remember to run update-grub on booting into '$sync_root'.$fg_reset"
-fi
+sudo tar -C $sync_root/ --overwrite -xpSJf $sync_enc_data
+sudo $chroot_arg update-grub
+sudo DEBIAN_FRONTEND=noninteractive $chroot_arg dpkg-reconfigure libdvd-pkg || true
 
 # Check for fprintd that may not be present on the target, then update PAM configuration.
 
-if [ ! -f /usr/share/pam-configs/fprintd ] || ! dpkg -s libpam-fprintd 2>/dev/null >/dev/null; then
+if [ ! -f $sync_root/usr/share/pam-configs/fprintd ] ||
+  ! dpkg -s $dpkg_root_arg libpam-fprintd 2>/dev/null >/dev/null; then
   echo -e "${fg_orange}Removing any changes related to fingerprint authentication.$fg_reset"
-  sudo rm -f /usr/share/pam-configs/fprintd /usr/local/share/pam-configs/fprintd.orig
+  sudo mv -f $sync_root/usr/share/pam-configs/fprintd \
+    $sync_root/usr/local/share/pam-configs/fprintd.bak 2>/dev/null || true
 fi
 echo -e "${fg_green}Updating PAM configuration.$fg_reset"
-sudo pam-auth-update --package --force
+sudo $chroot_arg pam-auth-update --package --force
+
+echo
+echo -e "${fg_green}ALL DONE. You may need to do some things manually if you skipped any of the"
+echo "options asked before (package installation, directory encryption). Apart from those, the"
+echo "Ubuntu Pro subscription may need to be attached (check with 'pro status' in the restored"
+echo "system), if required. Else remove 'ubuntu-esm-*' files in /etc/apt/sources.list.d directory."
+echo -e $fg_reset
