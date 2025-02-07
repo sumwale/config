7a1ee95bf7eabf759188b196c9bcf430a42a3dd2
Sumedh Wale <sumwale@gmail.com>
2025-02-07 18:01:08 +0530

    updates for mprsync project and some other changes
    - added alias for mprsync project directory
    - exclude venv used by mprsync project
    - restrict systemd-inhibit --what to sleep and shutdown
    - add systemd-inhibit in borgmatic-launch.sh when executing borgmatic
    - exit with error when borgmatic fails in borgmatic-launch.sh after showing the
      failure notification

--- .local/bin/borgmatic-launch.sh
+++ .local/bin/borgmatic-launch.sh
@@ -17,10 +17,11 @@ borgmatic-setup.sh
 start=$(date +'%s.%N')
 notify-send -i document-save -u normal -t 10000 "Backup started" "Started on $(date +'%b %d %H:%M:%S'). Status can be seen with 'systemctl --user status borgmatic-backup.service'"
 
-if borgmatic -c $HOME/.config/borgmatic/config.yaml "$@"; then
+if systemd-inhibit --who="borgmatic" --what="sleep:shutdown" --why="Prevent interrupting backup" borgmatic -c $HOME/.config/borgmatic/config.yaml "$@"; then
   end=$(date +'%s.%N')
   elapsed=$(bc -l <<< "scale=2; ($end - $start + 0.005) / 1")
   notify-send -i document-save -u normal -t 10000 "Backup finished" "Finished on $(date +'%b %d %H:%M:%S') in $elapsed secs. Detailed logs can be seen with 'journalctl -t borgmatic'"
 else
   notify-send -i document-save -u critical -t 0 "Backup failed" "Check 'journalctl -t borgmatic' for details: $(journalctl -t borgmatic | tail -n5)"
+  exit 1
 fi
