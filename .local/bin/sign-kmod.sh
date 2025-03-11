f3d6dc001c4e80cb53b1283ed1cf7a0c7aa5b4d4
Sumedh Wale <sumwale@gmail.com>
2025-03-11 08:27:42 +0530

    added sign-kmod.sh script to explicitly sign kernel modules for secure boot
    the new script is used to sign out of kernel and non-dkms modules like vmware
    
    also exclude fstab and crypttab in restore

---
+++ .local/bin/sign-kmod.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# sign a kernel module for ubuntu/debian-like distributions
+
+set -e
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <module>"
+  exit 1
+fi
+
+mok_cert1=/var/lib/shim-signed/mok/MOK.der
+mok_cert2=/var/lib/dkms/mok.pub
+if [ -e $mok_cert1 ]; then
+  mok_cert=$mok_cert1
+  mok_key=/var/lib/shim-signed/mok/MOK.priv
+elif [ -e $mok_cert2 ]; then
+  mok_cert=$mok_cert2
+  mok_key=/var/lib/dkms/mok.key
+else
+  echo "Neither $mok_cert1 nor $mok_cert2 found!"
+  exit 1
+fi
+
+if [ -x /usr/bin/kmodsign ]; then
+  signer=/usr/bin/kmodsign
+else
+  kver="`uname -r`"
+  signer="`find -L "/lib/modules/$kver" -name sign-file | head -n1`"
+  if [ -z "$signer" ]; then
+    echo "Neither /usr/bin/kmodsign nor sign-file found!"
+    exit 1
+  fi
+fi
+
+sudo "$signer" sha512 $mok_key $mok_cert "$1"
