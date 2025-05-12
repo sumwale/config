41f23cc5521cc719bce93581b3bad7369e45e641
Sumedh Wale <sumwale@gmail.com>
2025-05-12 13:47:55 +0530

    share .config/dconf in ybox containers and more ybox config changes

--- .local/bin/launch-workspace-app.sh
+++ .local/bin/launch-workspace-app.sh
@@ -8,7 +8,7 @@ if [ -n "$ws_znum" ]; then
     input_key_num="`expr "$ws_znum" + 2 2>/dev/null`"
     # simulate <Alt>+{num} keypress (somehow single cmd with -d does not work reliably)
     if [ -n "$input_key_num" ]; then
-      ydotool key 56:1 && sleep 0.3 && \
+      ydotool key 56:1 && sleep 0.5 && \
         ydotool key -d 50 $input_key_num:1 $input_key_num:0 56:0
     fi
   else
