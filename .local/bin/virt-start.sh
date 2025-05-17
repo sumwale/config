d5ac60bc20d0fee126587c24558f88761f0079e3
Sumedh Wale <sumwale@gmail.com>
2025-05-18 04:42:17 +0530

    updated virt-start.sh/virt-stop.sh for latest libvirt service names

--- .local/bin/virt-start.sh
+++ .local/bin/virt-start.sh
@@ -9,7 +9,7 @@ if [ -x /etc/init.d/vmware ]; then
 else
   sudo modprobe kvm_intel
 
-  for service in qemu-kvm.service libvirtd-ro.socket libvirtd.socket libvirtd-admin.socket virtlockd-admin.socket virtlogd-admin.socket virtlogd.socket virtlockd.socket libvirt-guests.service libvirtd.service rpcbind.service nfs-kernel-server.service nfs-server.service smb.service nmb.service smbd.service nmbd.service ssh.service sshd.service; do
+  for service in qemu-kvm.service libvirtd-ro.socket libvirtd.socket libvirtd-admin.socket virtlockd-admin.socket virtlogd-admin.socket virtlogd.socket virtlockd.socket virtlogd.service virtlockd.service libvirt-guests.service libvirtd.service rpcbind.service nfs-kernel-server.service nfs-server.service smb.service nmb.service smbd.service nmbd.service ssh.service sshd.service; do
     if sudo systemctl list-unit-files $service; then
       sudo systemctl restart $service
     fi
--- .local/bin/virt-stop.sh
+++ .local/bin/virt-stop.sh
@@ -8,7 +8,7 @@ if [ -x /etc/init.d/vmware ]; then
   sudo rmmod vmmon vmnet 2>/dev/null
   sudo rmmod vmw_vsock_vmci_transport vmw_vmci vsock
 else
-  for service in sshd.service ssh.service nmbd.service smbd.service nmb.service smb.service nfs-server.service nfs-kernel-server.service rpcbind.service virtlogd-admin.socket virtlockd-admin.socket libvirtd-admin.socket libvirtd.socket virtlogd.service virtlogd.socket virtlockd.socket libvirtd-ro.socket libvirt-guests.service libvirtd.service; do
+  for service in sshd.service ssh.service nmbd.service smbd.service nmb.service smb.service nfs-server.service nfs-kernel-server.service rpcbind.service virtlogd-admin.socket virtlockd-admin.socket libvirtd-admin.socket libvirtd.socket virtlogd.service virtlogd.socket virtlockd.socket libvirtd-ro.socket libvirt-guests.service virtlockd.service virtlogd.service libvirtd.service; do
     if sudo systemctl list-unit-files $service; then
       sudo systemctl stop $service 2>/dev/null
       sudo systemctl disable $service 2>/dev/null
@@ -16,5 +16,5 @@ else
     fi
   done
 
-  #sudo rmmod kvm_intel kvm
+  #sudo rmmod kvm_intel kvm || /bin/true
 fi
