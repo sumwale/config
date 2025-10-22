9d86521de72834b55c8a396da426683c6e37828f
Sumedh Wale <sumwale@gmail.com>
2025-10-22 11:27:43 +0530

    removed setpriv call in some apps; update launch-workspace-app.sh for KDE 6.5
    - removed setpriv call from steam, chromium etc since it is added by default by apps now
    - "wmctrl -d" no longer works with KDE Plasma 6.5, so use qdbus call to get current
      desktop instead for KDE session
    - add openssh_agent to the list of sockets to be checked for SSH_AUTH_SOCK

--- .local/bin/launch-workspace-app.sh
+++ .local/bin/launch-workspace-app.sh
@@ -2,10 +2,15 @@
 
 set -e
 
-ws_znum="`wmctrl -d | awk '{ if ($2 == "*") print $1 }'`"
+if [ "$DESKTOP_SESSION" = plasma ]; then
+  ws_znum="`qdbus6 org.kde.KWin /KWin currentDesktop`"
+else
+  ws_znum="`wmctrl -d | awk '{ if ($2 == "*") print $1 }'`"
+  ws_znum="`expr "$ws_znum" + 1 2>/dev/null`"
+fi
 if [ -n "$ws_znum" ]; then
   if type ydotool 2>/dev/null >/dev/null; then
-    input_key_num="`expr "$ws_znum" + 2 2>/dev/null`"
+    input_key_num="`expr "$ws_znum" + 1 2>/dev/null`"
     # simulate <Alt>+{num} keypress (somehow single cmd with -d does not work reliably)
     if [ -n "$input_key_num" ]; then
       ydotool key 56:1 && sleep 0.5 && \
