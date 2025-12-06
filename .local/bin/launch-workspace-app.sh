f74ad03a8a20fc7a91e94321715993a282dc36ac
Sumedh Wale <sumwale@gmail.com>
2025-12-06 19:20:29 +0530

    show a notification in launch-workspace-app.sh
    - show notification about workspace app being launched in launch-workspace-app.sh
    - increased sleep between keystrokes for ydotool to make it more reliable

--- .local/bin/launch-workspace-app.sh
+++ .local/bin/launch-workspace-app.sh
@@ -8,16 +8,17 @@ else
   ws_znum="`wmctrl -d | awk '{ if ($2 == "*") print $1 }'`"
   ws_znum="`expr "$ws_znum" + 1 2>/dev/null`"
 fi
+notify-send -i tools -u normal -t 2000 "Launching workspace app" \
+  "Launching workspace app for desktop $ws_znum"
 if [ -n "$ws_znum" ]; then
+  input_key_num="`expr "$ws_znum" + 1 2>/dev/null`"
   if type ydotool 2>/dev/null >/dev/null; then
-    input_key_num="`expr "$ws_znum" + 1 2>/dev/null`"
     # simulate <Alt>+{num} keypress (somehow single cmd with -d does not work reliably)
     if [ -n "$input_key_num" ]; then
       ydotool key 56:1 && sleep 0.5 && \
-        ydotool key -d 50 $input_key_num:1 $input_key_num:0 56:0
+        ydotool key -d 80 $input_key_num:1 $input_key_num:0 56:0
     fi
   else
-    input_key_num="`expr "$ws_znum" + 1 2>/dev/null`"
     if [ -n "$input_key_num" ]; then
       xdotool key --clearmodifiers --delay 100 "alt+$input_key_num"
     fi
